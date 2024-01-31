71b7b1a1f72c0be9aa88ead88ef09ecab84fc5a9
Akash Patro <akashpatro490@gmail.com>
2024-01-31 19:13:08 +0530

    rolled-back

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -12,7 +12,7 @@ const Body = () => {
    const [filteredResList, setFilteredResList] = useState([]);  
    const [topButton, setTopButton] = useState(true);   
    
-   // const LabelCard = LabelledResCard(ResCards);
+   const LabelCard = LabelledResCard(ResCards);
    
 
 
@@ -84,11 +84,11 @@ const Body = () => {
          {filteredResList?.map((restaurant) => (           
             <Link key={restaurant?.info?.id} to={"/restaurants/"+ restaurant?.info?.id} >
 
-             {/* {restaurant?.info?.avgRating > 4.3 ? <LabelCard resData={restaurant} /> : 
+             {restaurant?.info?.avgRating > 4.3 ? <LabelCard resData={restaurant} /> : 
              <ResCards  resData={restaurant} />      
-             }    */}
+             }   
 
-             <ResCards  resData={restaurant} />             
+             {/* <ResCards  resData={restaurant} /> */}
 
             </Link>
          ))   
